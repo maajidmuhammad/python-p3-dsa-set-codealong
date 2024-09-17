0b551a1e7bb46e5975bd87acb77c2a89f51ccd8b
maajidmuhammad <abdimohamed0742@gmail.com>
2024-09-17 12:40:13 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,24 @@
-class MySet:
 
-    pass
+class MySet:
+    def __init__(self, elements=None):
+        """Initialize the set with an optional list of elements."""
+        if elements is None:
+            self.elements = set()
+        else:
+            self.elements = set(elements)
+    
+    def add(self, element):
+        """Add an element to the set."""
+        self.elements.add(element)
+    
+    def delete(self, element):
+        """Remove an element from the set if it exists."""
+        self.elements.discard(element)  # discard() won't raise an error if the element doesn't exist
+    
+    def has(self, element):
+        """Check if an element is in the set."""
+        return element in self.elements
+    
+    def size(self):
+        """Return the number of elements in the set."""
+        return len(self.elements)
